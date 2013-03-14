d1a70782cad0ac95a2cce4d510f0d30f5e736086
Chris Fidao <fideloper@gmail.com>
2013-03-14 15:03:53 -0700

    pagination

--- app/views/content/home.blade.php
+++ app/views/content/home.blade.php
@@ -8,14 +8,7 @@
     {{ $article->excerpt }}
   </article><!-- end article -->
   @endforeach
-  <div class="pagi">
-    <ul>
-      <li class="default badge"><a href="#">Prev</a></li>
-      <li class="primary badge"><a href="#">2</a></li>
-      <li class="default badge"><a href="#">3</a></li>
-      <li class="default badge"><a href="#">4</a></li>
-      <li class="default badge"><a href="#">Next</a></li>
-    </ul>
-  </div>
+
+  {{ $articles->links() }}
 </section>
 @stop        
