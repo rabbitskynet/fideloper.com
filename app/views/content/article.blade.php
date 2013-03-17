df49f8c0ce837ba4caad06f16f45b60dedc14193
Chris Fidao <fideloper@gmail.com>
2013-03-17 15:23:40 -0700

    added disqus threads

--- app/views/content/article.blade.php
+++ app/views/content/article.blade.php
@@ -23,6 +23,24 @@
     <div class="content twelve columns">
         <h2 class="title">{{ $article->title }}</h2>
         {{ \Michelf\MarkdownExtra::defaultTransform($article->content) }}
+
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'fideloper'; // required: replace example with your Disqus site shortname
+            var disqus_url = 'http://fideloper.com/post/44396437701/laravel4-etag-conditional-get';
+            var disqus_title = 'ETag&#8217;s and Conditional GET&#8217;s in Laravel 4';
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+
     </div><!-- end content -->
   </div>
 </article><!-- end article -->
