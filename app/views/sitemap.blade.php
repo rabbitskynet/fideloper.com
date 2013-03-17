c28cbf66b1a709a550a1f67be9a2cf607db227fd
Chris Fidao <fideloper@gmail.com>
2013-03-17 16:12:03 -0700

    updated sitemap format for simplicity

--- app/views/sitemap.blade.php
+++ app/views/sitemap.blade.php
@@ -2,6 +2,6 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url><loc>http://fideloper.com/</loc><changefreq>hourly</changefreq><lastmod>{{ $latest->updated_at }}</lastmod></url>
 @foreach( $articles as $article )
-<url><loc>http://fideloper.com/{{ $article->url_title }}</loc><lastmod>{{ date('Y-m-d\TH:i:sP', strtotime($article->updated_at)) }}</lastmod></url>
+<url><loc>http://fideloper.com/{{ $article->url_title }}</loc><lastmod>{{ date('c', strtotime($article->updated_at)) }}</lastmod></url>
 @endforeach
 </urlset>
