6c2871f4fa9429e1c9b87e9f9b4cf1283c8bcfe2
Chris Fidao <fideloper@gmail.com>
2013-03-17 16:17:06 -0700

    finally fixed sitemap

--- app/views/sitemap.blade.php
+++ app/views/sitemap.blade.php
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url><loc>http://fideloper.com/</loc><changefreq>hourly</changefreq><lastmod>{{ $latest->updated_at }}</lastmod></url>
+<url><loc>http://fideloper.com/</loc><changefreq>hourly</changefreq><lastmod>{{ date('c', strtotime($latest->updated_at)) }}</lastmod></url>
 @foreach( $articles as $article )
 <url><loc>http://fideloper.com/{{ $article->url_title }}</loc><lastmod>{{ date('c', strtotime($article->updated_at)) }}</lastmod></url>
 @endforeach
