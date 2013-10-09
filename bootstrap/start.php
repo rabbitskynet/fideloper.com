7fd164a35c80dcbd65f753323199e4f251e2406c
fideloper <fideloper@gmail.com>
2013-10-09 09:05:01 -0700

    updated bootstrap env detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('fnew.ubuntu.local', 'localhost'),
+	'local' => array('*.local', 'localhost'),
 
 ));
 
